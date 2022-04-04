9df7376845333eea03370eb4d2c6553aa5f01c80
dinesh nangare <dineshnangare1717@gmail.com>
2022-04-05 00:29:40 +0530

    Emp Wage Salary added

--- src/main/java/OOPS/EmployeeWage.java
+++ src/main/java/OOPS/EmployeeWage.java
@@ -78,7 +78,39 @@ public class EmployeeWage {
                 break;
         }
     }
+    public void EmpSalary(){
+        int IsFullTime = 2;
+        int IsPartTime = 1;
+        int PerHourSalary = 20;
+        int workingHour =0;
+        int totalSalary = 0;
+        int Salary;
 
+        for (int day = 1; day <= 20; day++)
+        {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+            switch (empCheck)
+            {
+
+                case 0:
+                    workingHour = 8;
+                    break;
+
+                case 1:
+                    workingHour = 4;
+                    break;
+
+                case 2:
+                    workingHour = 0;
+                    break;
+            }
+            Salary =workingHour * PerHourSalary ;
+            totalSalary = totalSalary + Salary;
+
+        }
+        System.out.println("Employee has earned total salary in a month is: " + totalSalary);
+
+    }
 
     public static void main(String[] args) {
         System.out.println("welcome to employee wage");
@@ -88,5 +120,6 @@ public class EmployeeWage {
         object.CalculateDailyWage();
         object.PartTimeEmpWage();
         object.EmpWageSwitchCase();
+        object.EmpSalary();
     }
-}
+   }
