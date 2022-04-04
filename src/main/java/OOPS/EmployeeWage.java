1e9ab156ef040f8220b4a7d2ab89c03dd6efd14b
dinesh nangare <dineshnangare1717@gmail.com>
2022-04-05 00:21:20 +0530

    emp attendanceUC3

--- src/main/java/OOPS/EmployeeWage.java
+++ src/main/java/OOPS/EmployeeWage.java
@@ -2,7 +2,7 @@ package OOPS;
 import java.util.Scanner;
 
 public class EmployeeWage {
-    public void EmployeeAttendance(){
+    public void EmployeeAttendance() {
         int IS_FULL_TIME = 1;
         double empCheck = Math.floor(Math.random() * 10) % 2;
         if (empCheck == IS_FULL_TIME)
@@ -12,7 +12,7 @@ public class EmployeeWage {
 
     }
 
-    public void CalculateDailyWage(){
+    public void CalculateDailyWage() {
         System.out.println("Enter numbers of hours worked");
         Scanner scanner = new Scanner(System.in);
         double totalHoursWorked = scanner.nextInt();
@@ -30,11 +30,32 @@ public class EmployeeWage {
 
     }
 
+    public void PartTimeEmpWage() {
+        int EmpWagePerHr = 20;
+        int EmpHr = 8;
+        int IsFullTime = 1;
+        int IsPartTime = 2;
+        int Salary = 0;
+
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        if (empCheck == IsFullTime) {
+            Salary = (EmpHr * EmpWagePerHr);
+            System.out.println("Employee salary for full time is: " + Salary);
+        } else if (empCheck == IsPartTime) {
+            EmpHr = 4;
+            Salary = (EmpHr * EmpWagePerHr);
+            System.out.println("Employee salary for part time is: " + Salary);
+        } else
+            System.out.println("Employee salary for absent is :" + Salary);
+
+    }
+
     public static void main(String[] args) {
         System.out.println("welcome to employee wage");
         EmployeeWage object = new EmployeeWage();
 
         object.EmployeeAttendance();
         object.CalculateDailyWage();
+        object.PartTimeEmpWage();
     }
 }
